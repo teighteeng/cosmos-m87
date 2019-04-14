95c5ae5fb5b48ce28792372aa76ed9816eb45e8d
Thanapong Yingyong <thanapongtle@gmail.com>
2019-04-14 17:28:00 +0700

    npm i parallax-js

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
-    
+    <Main></Main>
+    <!-- <full-page ref="fullpage" :options="options" id="fullpage">
+    <div class="section">
+      First section ...
+    </div>
+    <div class="section">
+      Second section ...
+    </div>
+  </full-page> -->
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,5 +9,3 @@ export default {
   name: "HelloWorld",
 };
 </script>
-
-</style>
